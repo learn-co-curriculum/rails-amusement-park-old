746788555ff9d29f6f509693ebd817a70958e751
jacobshiff <jacobshiff@gmail.com>
2016-05-09 14:29:50 -0400

    Added new user and create actions to User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def index
+
   end
 
   def show
@@ -7,19 +8,30 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    @user.save
     @signup_active = "active"
+    session[:id] = @user.id
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to user_path(@user)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets)
   end
 
 end
