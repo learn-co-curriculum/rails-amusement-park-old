ab0f20d284cf33a2b2127dc259bc8c5811116c91
lianaluci <llgrobstein@gmail.com>
2016-05-09 14:49:12 -0400

    Create Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :update, :edit]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -11,20 +12,26 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     
+    redirect_to @user, notice: "Welcome to the theme park!"
   end
 
   def update
-    
+    @user.update(user_params)
+
+    redirect_to @user, notice: "User was successfully updated."
   end
 
   def edit
-    
   end
 
   private 
 
   def user_params
     params.require(:user).permit(:name, :nausea, :happiness, :tickets, :height)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
