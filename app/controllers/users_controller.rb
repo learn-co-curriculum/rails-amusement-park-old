a57f7bcb1d54aaca080d27785a7f8be751f989a7
jacobshiff <jacobshiff@gmail.com>
2016-05-09 14:03:28 -0400

    Created new user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def index
   end
 
@@ -6,6 +6,7 @@ class UserController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def edit
@@ -19,4 +20,5 @@ class UserController < ApplicationController
 
   def destroy
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/', to: 'welcome#index', as: "index"
+  get '/', to: 'welcome#index', as: "root"
   resources :attractions
   resources :users
 end
