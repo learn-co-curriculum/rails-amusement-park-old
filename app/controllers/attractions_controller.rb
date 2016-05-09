496378c738c9633d4ded6547fdbacb5c837541dc
lianaluci <llgrobstein@gmail.com>
2016-05-09 14:45:39 -0400

    Edit update action in AttractionController

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -26,7 +26,7 @@ class AttractionsController < ApplicationController
   end
 
   def update
-    @attraction.find(params[:id])
+    @attraction = Attraction.find(params[:id])
     @attraction.update(attraction_params)
 
     redirect_to @attraction, notice: "Attraction was successfully updated."
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+  end
+
+  def update
+    
+  end
+
+  def edit
+    
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name, :nausea, :happiness, :tickets, :height)
+  end
+
+end
