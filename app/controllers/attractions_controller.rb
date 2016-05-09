fe5c396370bba6792bef5d12cbccc078baa3708d
hdwatts <howard@hdwatts.com>
2016-05-09 16:32:40 -0400

    Added new attraction form

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -6,17 +6,25 @@ class AttractionsController < ApplicationController
   end
 
   def new
+    @attraction = Attraction.new
   end
 
   def edit
   end
 
   def create
+    @attraction = Attraction.create(params_require)
+    flash[:success] = "Congrats. You've sucessfully created an attraction."
+    redirect_to attractions_path(@attraction)
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def params_require
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
   end
 end
