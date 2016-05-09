bd20b6649ed51ddf9cf7fdc160ec21323390a496
jacobshiff <jacobshiff@gmail.com>
2016-05-09 13:48:20 -0400

    Added take_ride methods to User

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -2,4 +2,5 @@ class Attraction < ActiveRecord::Base
   # write associations here
   has_many :rides
   has_many :users, through: :rides
+
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,43 @@
 class Ride < ActiveRecord::Base
   belongs_to :attraction
   belongs_to :user
+
+  def take_ride
+    if !ticket_check && !height_check
+      "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif !ticket_check
+      "Sorry. You do not have enough tickets the #{attraction.name}."
+    elsif !height_check
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      ticket_update
+      nausea_update
+      happiness_update
+      user.save
+    end
+  end
+
+  private
+  def ticket_check
+    user.tickets >= attraction.tickets
+  end
+
+  def height_check
+    user.height >= attraction.min_height
+  end
+
+
+  def ticket_update
+    user.tickets -= attraction.tickets
+  end
+
+  def nausea_update
+    user.nausea += attraction.nausea_rating
+  end
+
+  def happiness_update
+    user.happiness += attraction.happiness_rating
+  end
+
+
 end
