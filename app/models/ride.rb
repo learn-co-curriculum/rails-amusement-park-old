f7ff15c278d997312c6c918a9f7634537ff141b8
erinpettigrew <erin.pettigrew@gmail.com>
2016-05-09 14:24:23 -0400

    Add models and methods for users, rides, attractions

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,17 @@
 class Ride < ActiveRecord::Base
   belongs_to :attraction
   belongs_to :user
+
+  def take_ride
+    if user.height >= min_height && user.tickets >= tickets
+      user.tickets -= attraction.tickets
+      user.happiness += attraction.happiness_rating
+      user.nausea += attraction.nausea_rating
+      flash[:error] = "Thanks for riding #{attraction.name}!"
+    elsif user.tickets < tickets
+      flash[:error] = "Sorry. You do not have enough tickets the #{attraction.name}."
+    elsif user.height < height
+      flash[:error] = "Sorry. You are not tall enough to ride the #{attraction.name}."
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
 
+  resources :users
+  resources :attractions
+
+  get '/', to: 'users#new'
 
 end
